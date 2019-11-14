09b39052c5f69fca7b93df46c2169f8607c6a2ff
Arek Donajski <arkadiusz.donajski@contracted.pmi.com>
2019-11-14 19:07:02 +0100

    subnets 1st version

--- main.tf
+++ main.tf
@@ -17,3 +17,27 @@ resource "aws_vpc" "main" {
     ]
   }
 }
+
+resource "aws_subnet" "main" {
+  
+  count = length(var.availability_zone_names)
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = cidrsubnet(var.cidr_block,length(var.availability_zone_names),count.index)
+  availability_zone = var.availability_zone_names[count.index]
+
+  tags = {
+      Managedby   = "Terraform"
+      Environment = var.environment
+      CreatedOn   = timestamp()
+      ChangedOn   = timestamp()
+      Module      = "tf_learn_vpc"
+      Project     = "learning"
+      ResourceType = "Subnet"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      tags["CreatedOn"],
+    ]
+  }
+}
